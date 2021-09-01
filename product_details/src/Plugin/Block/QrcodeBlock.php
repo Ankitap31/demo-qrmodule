bb3dba248840b8a8ed0303e48d8c13ad1264c4bd
Ankita Priyadarsini <ankita.priyadarsini@wipro.com>
2021-09-01 21:04:06 +0530

    product details module committed

---
+++ product_details/src/Plugin/Block/QrcodeBlock.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\product_details\Plugin\Block\QrcodeBlock.
+ */
+namespace Drupal\product_details\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use \Drupal\file\Entity\File;
+require_once DRUPAL_ROOT.'\vendor\pendalff\phpqrcode\qrlib.php';
+//use Drupal\pendalff\phpqrcode\qrlib;
+
+/**
+ * Provides a 'QR code' block.
+ *
+ * @Block(
+ *   id = "qr_block",
+ *   admin_label = @Translation("Scan Here To Purchase This Product"),
+ *   category = @Translation("Scan Here To Purchase This Product")
+ * )
+ */
+class QrcodeBlock extends BlockBase {
+    public function generateQrCodes($qr_text) {
+      // The below code will automatically create the path for the img.
+	  $node = \Drupal::routeMatch()->getParameter('node');
+	  $nid = $node->id();
+      $path = '';
+      $directory = "public://Images/QrCodes/";
+      file_prepare_directory($directory, FILE_MODIFY_PERMISSIONS | FILE_CREATE_DIRECTORY);
+      // Name of the generated image.
+      $uri = $directory . $nid . '.png'; // Generates a png image.
+      $path = drupal_realpath($uri);
+      // Generate QR code image.
+      \PHPQRCode\QRcode::png($qr_text, $path, 'L', 4, 2);
+      return path;
+    }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+	  drupal_flush_all_caches();
+	  $node = \Drupal::routeMatch()->getParameter('node');
+		if ($node instanceof \Drupal\node\NodeInterface) {
+		  // You can get nid and anything else you need from the node object.
+		  $nid = $node->id();
+		  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+		  $node_detail = $node_storage->load($nid);
+		  $node_detail->get('field_purchase_link')->uri;
+		}
+		$qrimage = "public://Images/QrCodes/".$nid.'.png';
+		if (file_exists($qrimage) == 0) {
+        QrcodeBlock::generateQrCodes($node_detail->get('field_purchase_link')->uri);
+		}
+		//include("C:/xampp/htdocs/drupal8/vendor/pendalff/phpqrcode/qrlib.php");
+		//QRcode::png($text, $qrpath);
+		//require_once 'C:\xampp\htdocs\drupal8\vendor\pendalff\phpqrcode\qrlib.php';
+		//\PHPQRCode\QRcode::png($node_detail->get('field_purchase_link')->uri);
+		$qrcode = file_create_url($qrimage);
+    return array(
+      '#type' => 'markup',
+      '#markup' => $qrimage."<img src='".$qrcode."'/>",
+    );
+  }
+
+  /**
+   * @return int
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+}
